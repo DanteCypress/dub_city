a4a777dd7d1b7082f86f154d5ad743acc9e27060
Dante Moore <dante@Dantes-MBP.attlocal.net>
2019-06-28 12:35:32 -0700

    Updates

---
+++ precache-manifest.e37997b0ddcb1d5fca53a6dc7a92dcea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a1df6096b61631d56e5364bce0f6c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96ebfc47d683396c9ed0",
+    "url": "/static/css/main.875460fa.chunk.css"
+  },
+  {
+    "revision": "9abf7892d67cdc7354e8",
+    "url": "/static/js/2.19bb8721.chunk.js"
+  },
+  {
+    "revision": "96ebfc47d683396c9ed0",
+    "url": "/static/js/main.c6577017.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "643bdb48540f70aed4c55ce2e3cee473",
+    "url": "/static/media/GSW.643bdb48.png"
+  },
+  {
+    "revision": "3756195b0e96acbffe5ad6a50702fe26",
+    "url": "/static/media/GSWB.3756195b.png"
+  },
+  {
+    "revision": "4bdb714412581f47ba71d5515fd8af3a",
+    "url": "/static/media/KD.4bdb7144.png"
+  },
+  {
+    "revision": "8f7c0b7f6f03ccb252cc15d54e6acdc8",
+    "url": "/static/media/curry.8f7c0b7f.png"
+  },
+  {
+    "revision": "f5caa6bf7ed19e31733567cb260ccfef",
+    "url": "/static/media/jersey.f5caa6bf.png"
+  }
+]);
